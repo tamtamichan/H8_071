06defbe4fde49a02424c48b351f9a2bb3227cac7
tamtamichan <hutami@ksei.co.id>
2022-04-12 23:30:52 +0700

    add Sesi 3 and README

---
+++ README.md
@@ -0,0 +1,5 @@
+Introduction to Python for Data Science
+Batch 71
+Hutami Laksmi
+hutami@ksei.co.id
+PYTN075ONL005
---
+++ mod.py
@@ -0,0 +1,17 @@
+s = "Hacktiv8-PTP Python For Data Science"
+a = [100, 200, 300]
+print('a =', a)
+
+def foo(arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass
+
+if(__name__ == '__main__'):
+    print('Executing as standalone script')
+    print(s)
+    print(a)
+    foo('quux')
+    x = Foo()
+    print(x)
